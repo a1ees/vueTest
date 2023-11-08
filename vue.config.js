3a5413a4705f94510a9f094b2b710ce7328b154f
alees <nprolol8@gmail.com>
2023-11-08 20:17:37 +0300

    первый коммит

---
+++ vue.config.js
@@ -0,0 +1,17 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { defineConfig } = require("@vue/cli-service");
+
+module.exports = defineConfig({
+  transpileDependencies: true,
+  configureWebpack: {
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: "css/[name].css",
+        chunkFilename: "css/[name].css",
+      }),
+    ],
+  },
+});
+
+
+
